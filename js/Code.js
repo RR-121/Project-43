e6d4cb55545b1c0b53c4d421964aee69bb92bd4b
RR-121 <74299785+RR-121@users.noreply.github.com>
2021-06-26 21:08:40 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,21 @@
+const accessCode1 = "ARRAYS";
+const accessCode2 = "DEBUGGER";
+const accessCode3 = "DEFAULT";
+
+function clues() {
+    fill("white");
+    textSize(15);
+    text("R Y A R S A", 100, 50);
+    fill("lightblue");
+    text("Hint: Can store multiple data types in one go!!", 100, 70);
+
+    fill("white");
+    text("B E R G D U G E", 700, 150);
+    fill("lightblue");
+    text("Hint: Helps find out mistakes!", 700, 170);
+
+    fill("white");
+    text("F D L A T U E", 300, 350);
+    fill("lightblue");
+    text("Hint: The original version of something!", 300, 370);
+}
